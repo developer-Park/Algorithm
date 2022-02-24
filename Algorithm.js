2fa0f14ce646db5bb1a2b899be939a11a0fd0578
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-02-23 19:31:44 -0700

    Update Algorithm.js

--- Algorithm.js
+++ Algorithm.js
@@ -1,7 +1,27 @@
+// Basic input //
+
+console.log("Hello World!");
+
+
 //A+B Given two integers A and B, calculate their sum.//
 
+
 const fs = require('fs');
-const inputData = fs.readFileSync(0, 'utf8').toString().split(' ');
+const inputData = fs.readFileSync(0, 'utf8').toString().split('');
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A+B);
+
+//A+B Given two integers A and B, calculate their sum.//
+
+// A and B are different line //
+
+// Using a \n //
+
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').toString().split("\n");
 
 const A = parseInt(inputData[0]);
 const B = parseInt(inputData[1]);
