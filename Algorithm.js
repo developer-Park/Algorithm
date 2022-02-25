125f50a1317ec55a4b402e3062fe61c235761c9c
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-02-24 22:39:22 -0700

    input output

--- Algorithm.js
+++ Algorithm.js
@@ -179,3 +179,21 @@ for (let i = 0; i<A; i++) {
 
 console.log(sum);
 
+// 5   
+        //1
+        //2
+        //3
+        //4
+        //5
+
+        
+let input = Number(require('fs').readFileSync('/dev/stdin').toString());
+
+let answer = '';
+
+for (let i = 1; i <= input; i++) {
+    answer += i + "\n";
+}
+
+console.log(answer);
+
