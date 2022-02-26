c41e6da5579a02161733d8c0b6faf6c47d3b7413
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-02-26 12:51:56 -0700

    input output

--- Algorithm.js
+++ Algorithm.js
@@ -293,3 +293,25 @@ let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
 let n = parseInt(input);
 
 console.log((n * (n+1))/2);
+
+
+//5
+//20 10 35 30 7
+
+//min : 7 max : 35
+
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let count = Number(input[0]);
+let numberStr = input[1].split(' ');
+let numbers = numberStr.map(function(cur) {
+  return Number(cur);
+});
+let max = numbers[0];
+let min = numbers[0];
+
+for(let i=1; i<count; i++){
+  if(max < numbers[i]) max = numbers[i];
+  if(min > numbers[i]) min = numbers[i];    
+}
+console.log(`${min} ${max}`);
--- practice.js
+++ practice.js
@@ -1,5 +1,15 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-let n = parseInt(input);
+let count = Number(input[0]);
+let numberStr = input[1].split(' ');
+let numbers = numberStr.map(function(cur) {
+  return Number(cur);
+});
+let max = numbers[0];
+let min = numbers[0];
 
-console.log((n * (n+1))/2);
+for(let i=1; i<count; i++){
+  if(max < numbers[i]) max = numbers[i];
+  if(min > numbers[i]) min = numbers[i];    
+}
+console.log(`${min} ${max}`);
