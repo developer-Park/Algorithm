4f6d1a46c468563d9f78d1ab646374991656f72a
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-04 15:10:41 -0700

    11718.js

---
+++ 11718.js
@@ -0,0 +1,11 @@
+//11718
+
+//Hello           Hello
+//Baekjoon        Baekjoon 
+//Online Judge    Online Judge
+
+
+
+let input = require('fs').readFileSync('/dev/stdin').toString();
+
+console.log(input);
