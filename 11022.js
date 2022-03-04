16dc9158a4e6752e40d29a1ca2b1a2b53a1b4a50
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-04 15:09:52 -0700

    11022.js

---
+++ 11022.js
@@ -0,0 +1,23 @@
+//11022
+
+//5 
+//1 1 Case #1: 1 + 1 = 2
+//2 3 Case #2: 2 + 3 = 5
+//3 4 Case #3: 3 + 4 = 7
+//9 8 Case #4: 9 + 8 = 17
+//5 2 Case #5: 5 + 2 = 7
+
+
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').toString().split("\n");
+
+const T = inputData[0];
+
+for(let i = 1; i<=T; i++) {
+    let AB = inputData[i].split(' '); 
+    let A = parseInt(AB[0]);
+    let B = parseInt(AB[1]);
+   
+    console.log(`Case #${i}: ${A} + ${B} = ${A + B}`);
+  
+}
