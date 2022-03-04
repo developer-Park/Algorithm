7786f3fc953e31da6e76b1502f325a5d0ac726a4
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-04 14:53:11 -0700

    2440.js

---
+++ 2440.js
@@ -0,0 +1,24 @@
+//2440
+
+//5
+
+//*****
+//****
+//***
+//**
+//* 
+
+
+let input = require('fs').readFileSync('/dev/stdin').toString();
+
+let num = Number(input);
+
+for (let i = 0; i < num; i++) {
+  let star =  '';
+    
+  for (let j = i + 1; j <= num; j++) {
+    star += '*';
+  }
+  
+  console.log(star);
+}
