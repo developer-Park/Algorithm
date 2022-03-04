72d35ea671ebb6b138dcc70e8c5b6728b00ad777
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-04 15:03:02 -0700

    10950.js

---
+++ 10950.js
@@ -0,0 +1,26 @@
+
+// 10950
+
+// A + B //
+//First line has Test case number T//
+// Each test case are one line, each line include A and B//
+//Example//
+//Input   //Output
+//5 (Total Test case)        
+//1 1       2
+//2 3       5
+//3 4       7
+//9 8       17
+//5 2       7
+
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').toString().split('\n');
+
+const T = inputData[0];
+
+for(let i = 1; i<=T; i++) {
+    let AB = inputData[i].split(' '); 
+
+
+    console.log(parseInt(AB[0]) + parseInt(AB[1]));
+}
