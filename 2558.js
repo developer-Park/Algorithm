db4b7acc2380ba2c2332e2917d2fc9516b2a9c01
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-04 15:02:21 -0700

    2558.js

---
+++ 2558.js
@@ -0,0 +1,15 @@
+//2558
+
+//A+B Given two integers A and B, calculate their sum.//
+
+// A and B are different line //
+
+// Using a \n //
+
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').toString().split("\n");
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A+B);
