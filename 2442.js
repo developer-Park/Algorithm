cc76fc310343e5f0f102c7a44ea1428b51c60050
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-04 14:52:22 -0700

    2442.js

---
+++ 2442.js
@@ -0,0 +1,19 @@
+//2442
+//5
+//    *
+//   ***
+//  *****
+// *******
+//*********
+
+const input = Number(require('fs').readFileSync('/dev/stdin').toString());
+
+
+let star = '';
+
+for(let i=1; i<=input; i++){
+  star += ' '.repeat(input - i) + '*'.repeat(i * 2 - 1) + '\n';
+
+}
+
+console.log(star);
