f9cb5ed3ae53606a7b04d3cfa38abf11925ceb65
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-03 22:11:49 -0700

    input output

--- Algorithm.js
+++ Algorithm.js
@@ -468,3 +468,29 @@ for (let i=1; i < input; i++) {
     stars += stars.split('').reverse().join('');
     console.log(stars);
 }
+
+
+//2522
+//3
+// *
+// **
+// ***
+// **
+// *
+
+
+const input = Number(require('fs').readFileSync('/dev/stdin').toString());
+
+let star ='';
+
+for(let i=1; i<=input; i++){
+  star += ' '.repeat(input - i) +  '*'.repeat(i) + '\n';
+    
+}
+
+for(let i =1; i<input; i++){
+    star += ' '.repeat(i) + '*'.repeat(input-i) + '\n';
+}
+
+
+console.log(star);
--- practice.js
+++ practice.js
@@ -1,13 +1,15 @@
 const input = Number(require('fs').readFileSync('/dev/stdin').toString());
 
-for (let i=1; i <= input; i++) {
-    stars = '*'.repeat(i) + ' '.repeat(input-i);
-    stars += stars.split('').reverse().join('');
-    console.log(stars);
+let star ='';
+
+for(let i=1; i<=input; i++){
+  star += ' '.repeat(input - i) +  '*'.repeat(i) + '\n';
+    
 }
 
-for (let i=1; i < input; i++) {
-    stars = '*'.repeat(input-i) + ' '.repeat(i);
-    stars += stars.split('').reverse().join('');
-    console.log(stars);
+for(let i =1; i<input; i++){
+    star += ' '.repeat(i) + '*'.repeat(input-i);
 }
+
+
+console.log(star);
