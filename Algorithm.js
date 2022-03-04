5283ac85d1dfe22701c8a954484c456a6cd8082f
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-03 22:36:13 -0700

    input output

--- Algorithm.js
+++ Algorithm.js
@@ -494,3 +494,34 @@ for(let i =1; i<input; i++){
 
 
 console.log(star);
+
+
+
+
+//2446
+//5
+// *********
+//  *******
+  // *****
+  //  ***
+    // *
+  //  ***
+  // *****
+//  *******
+// *********
+const input = Number(require('fs').readFileSync('/dev/stdin').toString());
+
+let star ='';
+
+for(let i=input; 0<i; i--){
+  star += ' '.repeat(input-i) +  '*'.repeat(2*i-1) + '\n';
+    
+}
+
+for(let i =2; i<=input; i++){
+    star += ' '.repeat(input-i) + '*'.repeat(2*i-1) + '\n';
+}
+
+
+console.log(star);
+
--- practice.js
+++ practice.js
@@ -2,14 +2,23 @@ const input = Number(require('fs').readFileSync('/dev/stdin').toString());
 
 let star ='';
 
-for(let i=1; i<=input; i++){
-  star += ' '.repeat(input - i) +  '*'.repeat(i) + '\n';
+for(let i=input; 0<i; i--){
+  star += ' '.repeat(input-i) +  '*'.repeat(2*i-1) + '\n';
     
 }
 
-for(let i =1; i<input; i++){
-    star += ' '.repeat(i) + '*'.repeat(input-i);
+for(let i =2; i<input; i++){
+    star += ' '.repeat(input-i) + '*'.repeat(2*i-1) + '\n';
 }
 
 
 console.log(star);
+
+
+
+
+9 7 5 3 1
+1 3 5 7 9 
+
+5+4 4+3 3+2 2+1 1+0 
+0    1   2   3   4
