529a3bb634f078b2b1dd97bf0f7533d05c779746
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-04 15:06:21 -0700

    10952.js

---
+++ 10952.js
@@ -0,0 +1,20 @@
+//10952
+
+//1 1       2
+//2 3       5
+//3 4       7
+//9 8       1
+//5 2       7
+//0 0 
+
+
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').toString().trim().split("\n");
+
+
+
+for(let i = 0; i < inputData.length - 1; i++) {
+    let AB = inputData[i].split(' '); 
+  
+    console.log(parseInt(AB[0]) + parseInt(AB[1]));
+}
