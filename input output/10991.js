dd028590504a855a830ee5f8895daf65fdf15d2a
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-03-10 21:56:02 -0700

    update

--- input output/10991.js
+++ input output/10991.js
@@ -12,14 +12,17 @@
 
 const input = Number(require('fs').readFileSync('/dev/stdin').toString());
 
-let star = '';
+
 
 for (let i = 0; i < input; i++) {
-    for (let j = 0; j < input - i - 1; j++)
+    let star = '';
+    for (let j = 0; j < input - i - 1; j++){
         star = star + ' ';
-    for (let j = 0; j < i + 1; j++)
+    }
+    for (let j = 0; j < i + 1; j++){
         star = star + "* ";
     star = star + "\n";
+    }
 }
 console.log(star);
 
--- input output/2439.js
+++ input output/2439.js
@@ -24,3 +24,54 @@ for (let i = 0; i < num; i++) {
   
   console.log(star);
 }
+
+
+
+
+const input = Number(require('fs').readFileSync('/dev/stdin').toString());
+
+
+
+for(let i = 0; i<input; i++) {
+  let star ='';
+  for (let j = 0; j<input-i; j++) {
+  star+= ' ';
+  }
+  for (let k = 0; k<i+1; k++) {
+    star+='*';
+    
+  }
+  console.log(star);
+  
+}
+
+
+
+let input = require('fs').readFileSync('/dev/stdin').toString();
+
+
+for (let i = 0; i < input; i++) {
+  let star = '';
+
+  for (let j = 0; j < input-i-1; j++){
+    star += ' ';
+}
+  for (let j = 0; j <= i; j++) {
+    star += '*';
+  }
+  console.log(star);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
